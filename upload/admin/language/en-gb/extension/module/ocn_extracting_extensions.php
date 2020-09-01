6b15cddb64f442a06ca346aa435a4d7f7bce2f9e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2020-09-01 21:38:28 +0300

    Пробелы для языковых файлов

--- upload/admin/language/en-gb/extension/module/ocn_extracting_extensions.php
+++ upload/admin/language/en-gb/extension/module/ocn_extracting_extensions.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']        = '[OCN] Extracting Extensions';
+$_['heading_title']            = '[OCN] Extracting Extensions';
 
 // Tab
-$_['tab_search']           = 'Search';
-$_['tab_files']            = 'Files';
-$_['tab_info']             = 'Information';
+$_['tab_search']               = 'Search';
+$_['tab_files']                = 'Files';
+$_['tab_info']                 = 'Information';
 
 // Column
-$_['column_search_path']   = 'Path';
-$_['column_search_file']   = 'File';
-$_['column_file_name']     = 'Module name';
-$_['column_file_size']     = 'Size, KB';
-$_['column_file_date']     = 'Date of creation';
+$_['column_search_path']       = 'Path';
+$_['column_search_file']       = 'File';
+$_['column_file_name']         = 'Module name';
+$_['column_file_size']         = 'Size, KB';
+$_['column_file_date']         = 'Date of creation';
 $_['column_search_id']         = 'ID';
 $_['column_search_code']       = 'Code';
 $_['column_search_name']       = 'Name';
@@ -22,52 +22,52 @@ $_['column_search_status']     = 'Status';
 $_['column_search_date_added'] = 'Added';
 
 // Table
-$_['table_files']           = 'List of files';
-$_['table_modifications']   = 'Modification';
+$_['table_files']              = 'List of files';
+$_['table_modifications']      = 'Modification';
 
 // Data
-$_['data_version']         = '3.0.1.0';
-$_['data_author_name']     = 'Hkr';
-$_['data_author_link']     = 'https://forum.opencart.name/members/hkr.3/';
-$_['data_extension_link']  = 'https://forum.opencart.name/resources/59/';
+$_['data_version']             = '3.0.1.0';
+$_['data_author_name']         = 'Hkr';
+$_['data_author_link']         = 'https://forum.opencart.name/members/hkr.3/';
+$_['data_extension_link']      = 'https://forum.opencart.name/resources/59/';
 
 // Help
-$_['help_module_name']     = '(ex: ocn_extracting_extensions)';
+$_['help_module_name']         = '(ex: ocn_extracting_extensions)';
 
 // Buttons
-$_['button_update']        = 'Update';
-$_['button_extract']       = 'Extract';
-$_['button_remove']        = 'Delete';
-$_['button_clear']         = 'Clear';
-$_['button_search']        = 'Search';
+$_['button_update']            = 'Update';
+$_['button_extract']           = 'Extract';
+$_['button_remove']            = 'Delete';
+$_['button_clear']             = 'Clear';
+$_['button_search']            = 'Search';
 
 // Text
-$_['text_name']            = 'Extension name';
-$_['text_version']         = 'Extension version';
-$_['text_author']          = 'Extension author';
-$_['text_module_name']     = 'Module name';
-$_['text_extension']       = 'Extensions';
-$_['text_total']           = 'Total found:';
+$_['text_name']                = 'Extension name';
+$_['text_version']             = 'Extension version';
+$_['text_author']              = 'Extension author';
+$_['text_module_name']         = 'Module name';
+$_['text_extension']           = 'Extensions';
+$_['text_total']               = 'Total found:';
 
 // Success
-$_['success']              = 'Success!';
-$_['success_delete']       = 'The selected files deleted successfully!';
-$_['success_extract']      = 'Module {name} was successfully extract! Go to the Files tab to download the archive.';
+$_['success']                  = 'Success!';
+$_['success_delete']           = 'The selected files deleted successfully!';
+$_['success_extract']          = 'Module {name} was successfully extract! Go to the Files tab to download the archive.';
 
 // Warning
-$_['warning']              = 'Warning!';
+$_['warning']                  = 'Warning!';
 
 // Empty
-$_['empty_search']         = 'No items were found matching your criteria. Check the module name.';
-$_['empty_files']          = 'No files found. To unload the module go to the <b>Search</b> tab.';
+$_['empty_search']             = 'No items were found matching your criteria. Check the module name.';
+$_['empty_files']              = 'No files found. To unload the module go to the <b>Search</b> tab.';
 
 // Error
-$_['error_permission']     = 'You have no rights to control the module Module Extract!';
-$_['error_name_min']       = 'Module name must be more than 5 characters!';
-$_['error_extract']        = 'You must select at least one file!';
-$_['error_class_zip']      = 'Your host doesn\'t support ZipArchive, the file is not created!';
-$_['error_create_zip']     = 'An error occurred creating a zip-archive!';
-$_['error_select_extract'] = 'Select the files to extract!';
-$_['error_select_delete' ] = 'Select files to delete!';
-$_['error_download_file']  = 'Select file to download!';
-$_['error_download_not']   = 'Attempt to download is not a file or file does not exist!';
+$_['error_permission']         = 'You have no rights to control the module Module Extract!';
+$_['error_name_min']           = 'Module name must be more than 5 characters!';
+$_['error_extract']            = 'You must select at least one file!';
+$_['error_class_zip']          = 'Your host doesn\'t support ZipArchive, the file is not created!';
+$_['error_create_zip']         = 'An error occurred creating a zip-archive!';
+$_['error_select_extract']     = 'Select the files to extract!';
+$_['error_select_delete' ]     = 'Select files to delete!';
+$_['error_download_file']      = 'Select file to download!';
+$_['error_download_not']       = 'Attempt to download is not a file or file does not exist!';
--- upload/admin/language/ru-ru/extension/module/ocn_extracting_extensions.php
+++ upload/admin/language/ru-ru/extension/module/ocn_extracting_extensions.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']        = '[OCN] Extracting Extensions';
+$_['heading_title']            = '[OCN] Extracting Extensions';
 
 // Tab
-$_['tab_search']           = 'Поиск';
-$_['tab_files']            = 'Файлы';
-$_['tab_info']             = 'Информация';
+$_['tab_search']               = 'Поиск';
+$_['tab_files']                = 'Файлы';
+$_['tab_info']                 = 'Информация';
 
 // Column
-$_['column_search_path']   = 'Путь';
-$_['column_search_file']   = 'Файл';
-$_['column_file_name']     = 'Название модуля';
-$_['column_file_size']     = 'Размер, Кб';
-$_['column_file_date']     = 'Дата создания';
+$_['column_search_path']       = 'Путь';
+$_['column_search_file']       = 'Файл';
+$_['column_file_name']         = 'Название модуля';
+$_['column_file_size']         = 'Размер, Кб';
+$_['column_file_date']         = 'Дата создания';
 $_['column_search_id']         = '#';
 $_['column_search_code']       = 'Код';
 $_['column_search_name']       = 'Название';
@@ -22,52 +22,52 @@ $_['column_search_status']     = 'Статус';
 $_['column_search_date_added'] = 'Добавлен';
 
 // Table
-$_['table_files']           = 'Список файлов';
-$_['table_modifications']   = 'Модификация';
+$_['table_files']              = 'Список файлов';
+$_['table_modifications']      = 'Модификация';
 
 // Data
-$_['data_version']         = '3.0.1.0';
-$_['data_author_name']     = 'Hkr';
-$_['data_author_link']     = 'https://forum.opencart.name/members/hkr.3/';
-$_['data_extension_link']  = 'https://forum.opencart.name/resources/59/';
+$_['data_version']             = '3.0.1.0';
+$_['data_author_name']         = 'Hkr';
+$_['data_author_link']         = 'https://forum.opencart.name/members/hkr.3/';
+$_['data_extension_link']      = 'https://forum.opencart.name/resources/59/';
 
 // Help
-$_['help_module_name'    ] = '(например: ocn_extracting_extensions)';
+$_['help_module_name'    ]     = '(например: ocn_extracting_extensions)';
 
 // Buttons
-$_['button_update']        = 'Обновить';
-$_['button_extract']       = 'Извлечь';
-$_['button_remove']        = 'Удалить';
-$_['button_clear']         = 'Очистить';
-$_['button_search']        = 'Искать';
+$_['button_update']            = 'Обновить';
+$_['button_extract']           = 'Извлечь';
+$_['button_remove']            = 'Удалить';
+$_['button_clear']             = 'Очистить';
+$_['button_search']            = 'Искать';
 
 // Text
-$_['text_name']            = 'Наименование расширения';
-$_['text_version']         = 'Версия расширения';
-$_['text_author']          = 'Автор расширения';
-$_['text_module_name']     = 'Название модуля';
-$_['text_extension']       = 'Расширения';
-$_['text_total']           = 'Всего найдено:';
+$_['text_name']                = 'Наименование расширения';
+$_['text_version']             = 'Версия расширения';
+$_['text_author']              = 'Автор расширения';
+$_['text_module_name']         = 'Название модуля';
+$_['text_extension']           = 'Расширения';
+$_['text_total']               = 'Всего найдено:';
 
 // Success
-$_['success']              = 'Успешно!';
-$_['success_delete']       = 'Выбранные файлы успешно удалены!';
-$_['success_extract']      = 'Модуль {name} успешно извлечен! Перейдите на вкладку <b>Файлы</b> для скачивания архива.';
+$_['success']                  = 'Успешно!';
+$_['success_delete']           = 'Выбранные файлы успешно удалены!';
+$_['success_extract']          = 'Модуль {name} успешно извлечен! Перейдите на вкладку <b>Файлы</b> для скачивания архива.';
 
 // Warning
-$_['warning']              = 'Внимание!';
+$_['warning']                  = 'Внимание!';
 
 // Empty
-$_['empty_search']         = 'Ничего не найдено по Вашим критериям. Проверьте название модуля.';
-$_['empty_files']          = 'Файлов не найдено. Чтобы выгрузить модуль перейдите на вкладку <b>Поиск</b>.';
+$_['empty_search']             = 'Ничего не найдено по Вашим критериям. Проверьте название модуля.';
+$_['empty_files']              = 'Файлов не найдено. Чтобы выгрузить модуль перейдите на вкладку <b>Поиск</b>.';
 
 // Error
-$_['error_permission']     = 'У Вас нет прав для управления модулем Extracting Extensions!';
-$_['error_name_min']       = 'Название модуля должно быть больше 5 символов!';
-$_['error_extract']        = 'Необходимо выбрать хотя бы один файл!';
-$_['error_class_zip']      = 'Ваш хостинг не поддерживает ZipArchive, файл не создан!';
-$_['error_create_zip']     = 'Произошла ошибка создания zip-архива!';
-$_['error_select_extract'] = 'Выберите файлы для извлечения!';
-$_['error_select_delete']  = 'Выберите файлы для удаления!';
-$_['error_download_file']  = 'Выберите файл для скачивания!';
-$_['error_download_not']   = 'Попытка скачать не файл или файл не существует!';
+$_['error_permission']         = 'У Вас нет прав для управления модулем Extracting Extensions!';
+$_['error_name_min']           = 'Название модуля должно быть больше 5 символов!';
+$_['error_extract']            = 'Необходимо выбрать хотя бы один файл!';
+$_['error_class_zip']          = 'Ваш хостинг не поддерживает ZipArchive, файл не создан!';
+$_['error_create_zip']         = 'Произошла ошибка создания zip-архива!';
+$_['error_select_extract']     = 'Выберите файлы для извлечения!';
+$_['error_select_delete']      = 'Выберите файлы для удаления!';
+$_['error_download_file']      = 'Выберите файл для скачивания!';
+$_['error_download_not']       = 'Попытка скачать не файл или файл не существует!';
